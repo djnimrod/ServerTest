5a2269555efe58de103b973d95c277bee8572f3c
danielDev <valcyum.085@gmail.com>
2020-11-20 22:15:57 -0400

    cambios en angular index

--- index.js
+++ index.js
@@ -1,9 +1,9 @@
 const express = require('express');
 const app = express();
 
-const port = process.env.PORT || 3000;
+const port = process.env.PORT || 4200;
 
-app.use(express.static(__dirname + '/docs/prod'));
+app.use(express.static(__dirname + '/proy/prod'));
 
 app.listen(port, () => {
   console.log(`Escuchando peticiones en el puerto ${port}`);
